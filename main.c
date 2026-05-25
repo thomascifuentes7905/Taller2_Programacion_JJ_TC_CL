88b43ad5dfd86414305698209b69cb181d0d4899
thomascifuentes7905 <thomas.cifuentes@udla.edu.ec>
2026-05-24 20:03:57 -0500

    ingreso de datos de la matris, notas por cada estudiante

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int estudiantes,asignaturas=3;
+    printf("Ingrese la cantidad de estudiantes: ");
+    scanf("%d",&estudiantes);
+
+    float notas[estudiantes][3];
+
+
+     // Ingreso de datos
+    for (int i = 0; i < estudiantes; i++) {
+        printf("\nEstudiante %d", i + 1);
+        for (int j = 0; j < asignaturas; j++) {
+            printf(" \n Asignatura %d: ", j + 1);
+            scanf("%f", &notas[i][j]);
+            if (notas[i][j] < 0 || notas[i][j] > 10) {
+                printf("  Error: ingrese un valor entre 0 y 10\n");
+                j--;
+            }
+        }
+    }
+    return 0;
+    
+    }
